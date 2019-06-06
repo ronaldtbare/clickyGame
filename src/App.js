fa5d8985dd0f1349b02dd591d1d4b9f50e2daa23
travisbare <travisbare@gmail.com>
2019-06-06 15:33:29 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from './components/card'
 import './App.css';
 
 function App() {
+  let score = 0;
+  let highscore = 0;
+  let message = "Start Picking";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <h2>Clicky Game!</h2>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <h4>{message}</h4>
+            </li>
+            <li class="nav-item">
+              <h4 className="scores">"Score: {score} | High Score: {highscore}</h4>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Clicky Game!</h1>
+          <p class="lead">Click on an image to earn points, but don't click on any more than once!</p>
+        </div>
+      </div>
+
+      <div class="picwrap">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div> 
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Card() {
+    return (
+        <div className="card" style={{width: "18rem"}}>
+            <img src="./src/images/portrait5.jpg" class="card-img-top" alt="portrait"></img>
+        </div>
+    );
+}
+export default Card;
